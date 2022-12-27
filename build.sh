ee4e91f5e1432efc4475e6a58788d35f724b71af
Kleis Auke Wolthuizen <github@kleisauke.nl>
2022-12-27 13:27:10 +0100

    Release v8.14.0-rc1

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ $# -lt 1 ]; then
   echo "Usage: $0 VERSION [PLATFORM]"
   echo "Build shared libraries for libvips and its dependencies via containers"
   echo
-  echo "Please specify the libvips VERSION, e.g. 8.13.3"
+  echo "Please specify the libvips VERSION, e.g. 8.14.0-rc1"
   echo
   echo "Optionally build for only one PLATFORM, defaults to building for all"
   echo
