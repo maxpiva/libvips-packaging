57ff50510c792cda5706e7821f39b5ad650ed7e7
Maximo Piva <maximo.piva@gmail.com>
2025-08-03 05:32:01 +0000

    aa

--- build/posix-exp.sh
+++ build/posix-exp.sh
@@ -300,7 +300,10 @@ cd ${DEPS}/heif
 # Downgrade minimum required CMake version to 3.12 - https://github.com/strukturag/libheif/issues/975
 sed -i'.bak' "/^cmake_minimum_required/s/3.16.3/3.12/" CMakeLists.txt
 CFLAGS="${CFLAGS} -O3 -I${TARGET}/include" CXXFLAGS="${CXXFLAGS} -O3 -I${TARGET}/include" cmake -G"Unix Makefiles" \
-  -DCMAKE_TOOLCHAIN_FILE=${ROOT}/Toolchain.cmake -DCMAKE_INSTALL_PREFIX=${TARGET} -DCMAKE_INSTALL_LIBDIR=lib ${HEIFOPENJPEG} -DCMAKE_CXX_FLAGS="$(CXXFLAGS) -O3 ${HEIFJPEGLI}" -DCMAKE_BUILD_TYPE=Release  \
+  -DCMAKE_TOOLCHAIN_FILE=${ROOT}/Toolchain.cmake -DCMAKE_INSTALL_PREFIX=${TARGET} -DCMAKE_INSTALL_LIBDIR=lib ${HEIFOPENJPEG} \
+  -DCMAKE_CXX_FLAGS="$(CXXFLAGS) -O3 ${HEIFJPEGLI} -I${TARGET}/include" -DCMAKE_PREFIX_PATH=${TARGET}  \
+  -DCMAKE_C_FLAGS="$(CXXFLAGS) -O3 ${HEIFJPEGLI} -I${TARGET}/include" \
+  -DCMAKE_BUILD_TYPE=Release  \
   -DBUILD_SHARED_LIBS=FALSE -DBUILD_TESTING=0 -DENABLE_PLUGIN_LOADING=0 -DWITH_EXAMPLES=0 -DWITH_LIBDE265=0 -DWITH_X265=0
 make install/strip
 if [ "$PLATFORM" == "linux-arm" ]; then
